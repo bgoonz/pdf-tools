87ed9b91ce5b2accd219697399f8898c0620eb7f
Bryan C Guner <bryan.guner@gmail.com>
2021-11-27 02:35:06 +0000

    hi

--- pages/about.js
+++ pages/about.js
@@ -1,6 +1,5 @@
 import React from "react";
 import Head from "next/head";
-
 export default function about() {
   return (
     <>
@@ -8,8 +7,15 @@ export default function about() {
         <title>pdftoolz -Home</title>
       </Head>
       <div>
-        <h2>About</h2>
+        <br>
+        </br>
         <a href="https://github.com/bgoonz/pdf-tools">Github Repo</a>
+        <br>
+        </br>
+        <hr>
+        </hr>
+        <br>
+        </br>
         <img src="https://i.imgur.com/5JDpzgO.gif" />
       </div>
     </>
